067f9c60537c884b66bed751e21c8690d695e258
lexrupy <lexrupy@gmail.com>
2009-01-03 02:03:20 -0200

    Added a post install script to provide users a way to install the custom snippets

--- make_deb.sh
+++ make_deb.sh
@@ -13,14 +13,15 @@ cp ./mime/* ./build/tmp/gedit-gmate/usr/share/mime/packages/
 mkdir -p ./build/tmp/gedit-gmate/usr/lib/gedit-2/plugins
 cp -r ./plugins/* ./build/tmp/gedit-gmate/usr/lib/gedit-2/plugins/
 # Copy Snippets
-# mkdir -p ./build/tmp/gedit-gmate/usr/share/gedit-2/plugins/snippets
-# cp -r ./snippets/* ./build/tmp/gedit-gmate/usr/share/gedit-2/plugins/snippets/
+mkdir -p ./build/tmp/gedit-gmate/usr/share/gedit-2/gmate/snippets
+cp -r ./snippets/* ./build/tmp/gedit-gmate/usr/share/gedit-2/gmate/snippets/
+cp gmate-installsnippets.sh ./build/tmp/gedit-gmate/usr/share/gedit-2/gmate/
 # Copy Styles
 mkdir -p ./build/tmp/gedit-gmate/usr/share/gtksourceview-2.0/styles
 cp -r ./styles/* ./build/tmp/gedit-gmate/usr/share/gtksourceview-2.0/styles/
 # Copy documentation
-mkdir -p ./build/tmp/gedit-gmate/usr/share/doc/gedit-plugins
-cp README.rdoc ./build/tmp/gedit-gmate/usr/share/doc/gedit-plugins/GMATE-README.rdoc
+mkdir -p ./build/tmp/gedit-gmate/usr/share/gedit-2/gmate/doc
+cp README.rdoc ./build/tmp/gedit-gmate/usr/share/gedit-2/gmate/doc/
 # Make the deb package
 dpkg-deb -b ./build/tmp/gedit-gmate ./build
 # Remove temporary directory

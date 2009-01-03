b56f28d63188d72a21ce4e90e4b251fae434900c
lexrupy <lexrupy@gmail.com>
2009-01-03 01:47:31 -0200

    Removed more unused files, and updated package creating scritps and package scripts

--- make_deb.sh
+++ make_deb.sh
@@ -5,7 +5,7 @@ mkdir -p ./build/tmp/gedit-gmate
 cp -r DEBIAN ./build/tmp/gedit-gmate/
 # Copy language specs
 mkdir -p ./build/tmp/gedit-gmate/usr/share/gtksourceview-2.0/language-specs
-cp -r ./lang-specs/* ./build/tmp/gedit-gmate/usr/share/gtksourceview-2.0/language-specs/
+cp -r ./lang-specs/ruby.lang ./build/tmp/gedit-gmate/usr/share/gtksourceview-2.0/language-specs/ruby.lang.gmate
 # Copy mime type
 mkdir -p ./build/tmp/gedit-gmate/usr/share/mime/packages
 cp ./mime/* ./build/tmp/gedit-gmate/usr/share/mime/packages/
@@ -13,8 +13,8 @@ cp ./mime/* ./build/tmp/gedit-gmate/usr/share/mime/packages/
 mkdir -p ./build/tmp/gedit-gmate/usr/lib/gedit-2/plugins
 cp -r ./plugins/* ./build/tmp/gedit-gmate/usr/lib/gedit-2/plugins/
 # Copy Snippets
-mkdir -p ./build/tmp/gedit-gmate/usr/share/gedit-2/plugins/snippets
-cp -r ./snippets/* ./build/tmp/gedit-gmate/usr/share/gedit-2/plugins/snippets/
+# mkdir -p ./build/tmp/gedit-gmate/usr/share/gedit-2/plugins/snippets
+# cp -r ./snippets/* ./build/tmp/gedit-gmate/usr/share/gedit-2/plugins/snippets/
 # Copy Styles
 mkdir -p ./build/tmp/gedit-gmate/usr/share/gtksourceview-2.0/styles
 cp -r ./styles/* ./build/tmp/gedit-gmate/usr/share/gtksourceview-2.0/styles/
@@ -24,4 +24,4 @@ cp README.rdoc ./build/tmp/gedit-gmate/usr/share/doc/gedit-plugins/GMATE-README.
 # Make the deb package
 dpkg-deb -b ./build/tmp/gedit-gmate ./build
 # Remove temporary directory
-rm -rf ./build/tmp
+#rm -rf ./build/tmp

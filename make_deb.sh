77b4176829fdacc3df7483ed9e9130ba147bf5d9
Lexrupy <lexrupy@gmail.com>
2009-02-20 10:30:37 -0300

    Updated make_deb.sh

--- make_deb.sh
+++ make_deb.sh
@@ -5,7 +5,11 @@ mkdir -p ./build/tmp/gedit-gmate
 cp -r DEBIAN ./build/tmp/gedit-gmate/
 # Copy language specs
 mkdir -p ./build/tmp/gedit-gmate/usr/share/gtksourceview-2.0/language-specs
-cp -r ./lang-specs/ruby.lang ./build/tmp/gedit-gmate/usr/share/gtksourceview-2.0/language-specs/ruby.lang.gmate
+cp ./lang-specs/ruby.lang ./build/tmp/gedit-gmate/usr/share/gtksourceview-2.0/language-specs/ruby.lang.gmate
+cp ./lang-specs/haml.lang ./build/tmp/gedit-gmate/usr/share/gtksourceview-2.0/language-specs/
+cp ./lang-specs/rhtml.lang ./build/tmp/gedit-gmate/usr/share/gtksourceview-2.0/language-specs/
+cp ./lang-specs/sass.lang ./build/tmp/gedit-gmate/usr/share/gtksourceview-2.0/language-specs/
+cp ./lang-specs/yml.lang ./build/tmp/gedit-gmate/usr/share/gtksourceview-2.0/language-specs/
 # Copy mime type
 mkdir -p ./build/tmp/gedit-gmate/usr/share/mime/packages
 cp ./mime/* ./build/tmp/gedit-gmate/usr/share/mime/packages/

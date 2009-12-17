618dd87d5c3843449691d2181a9ecb18d73d0f63
Sergey Rublev <narma.nsk@gmail.com>
2009-12-17 08:16:14 +0600

    Now support allmost gconf types, and options list.

--- plugins/gconfoptions.py
+++ plugins/gconfoptions.py
@@ -3,6 +3,25 @@
 import os
 import gconf
 
+GCONF_TYPES = (
+    gconf.VALUE_STRING,
+    gconf.VALUE_INT,
+    gconf.VALUE_BOOL,
+    gconf.VALUE_LIST,
+    gconf.VALUE_FLOAT,
+    gconf.VALUE_SCHEMA
+)
+
+class OptionsType(type):
+
+    def __new__(cls, base, name, attrs):
+        attrs['options'] = {}
+        for key, attr in attrs.iteritems():
+            if isinstance(attr, Option):
+                attrs['options'][key] = attr
+        new_class = super(OptionsType, cls).__new__(cls, base, name, attrs)
+        return new_class
+
 class Option(object):
 
     def __init__(self, key, default_value=None):
@@ -10,6 +29,8 @@ class Option(object):
         self.default_value = default_value
 
     def __get__(self, instance, owner=None):
+        if instance is None:
+            raise ValueError("Can't access to field without instance")
         key = os.path.join(instance.gconfDir, self.key)
         value = instance.client.get(key)
         if value is None and self.default_value is not None:
@@ -23,9 +44,17 @@ class Option(object):
         instance.client.set(key, value)
 
     def __delete__(self, instance):
-        instance.client.unset(self.key)
+        instance.client.unset(os.path.join(instance.gconfDir, self.key))
 
 class SimpleOption(Option):
+
+    gconf_type = gconf.VALUE_INVALID
+
+    def __init__(self, *args, **kwargs):
+        self.getter = getattr(gconf.Value, 'get_%s' % self.gconf_type.value_nick)
+        self.setter = getattr(gconf.Value, 'set_%s' % self.gconf_type.value_nick)
+        super(SimpleOption, self).__init__(*args, **kwargs)
+
     def __get__(self, instance, owner=None):
         gconf_value = super(SimpleOption, self).__get__(instance, owner)
         if gconf_value is None:
@@ -33,15 +62,25 @@ class SimpleOption(Option):
         return self.getter(gconf_value)
 
     def __set__(self, instance, value):
-        gconf_value = gconf.Value(gconf.VALUE_STRING)
+        gconf_value = gconf.Value(self.gconf_type)
         self.setter(gconf_value, value)
         super(SimpleOption, self).__set__(instance, gconf_value)
 
-class StringOption(SimpleOption):
-    getter = lambda self, o: gconf.Value.get_string(o)
-    setter = lambda self, o, v: gconf.Value.set_string(o, v)
+# Autogenerate classes StringOption, IntOption etc. with SimpleOption
+# same as:
+# class StringOption(SimpleOption):
+#     gconf_type = gconf.VALUE_STRING
+#
+for gconf_type in GCONF_TYPES:
+    class_name = '%sOption' % gconf_type.value_nick.capitalize()
+    new_class = type(class_name, (SimpleOption,), {'gconf_type': gconf_type})
+    globals()[class_name] = new_class
+
 
 class Options(object):
+
+    __metaclass__ = OptionsType
+
     def __init__(self, gconfDir):
         self.gconfDir = gconfDir
         self.client = gconf.client_get_default()
